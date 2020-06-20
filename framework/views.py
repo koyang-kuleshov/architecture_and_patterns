436dc7cc1dfcb14a294519cd54000a7bbd6a0ecd
koyang-kuleshov <51748535+koyang-kuleshov@users.noreply.github.com>
2020-06-20 15:56:34 +0300

    Удалил views.py из корня

--- framework/views.py
+++ framework/views.py
@@ -1,13 +1,15 @@
-from framework.framework import Application
+from framework import Application
 
 
 def index_view(request):
     return '200 OK', 'Index Page'
 
+
 def about_view(request):
     if 'secret' in request:
         return '200 OK', f'<h1>About Page {request["secret"]}</h1>'
     return '200 OK', f'<h1>About Page</h1>'
+
 
 urls = {
     '/': index_view,
@@ -15,9 +17,9 @@ urls = {
 }
 
 
-
 def secret_middleware(request):
     request['secret'] = 'secret'
+
 
 # middlewares = [secret_middleware]
 middlewares = []
